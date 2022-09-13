be0aab6dd71b4ee11390affa14b9f23f61981c55
Nia Watson <nia.d.watson@gmail.com>
2022-09-12 21:01:26 -0500

    feat Data Types

---
+++ freecodecamp/basic-js/variables.js
@@ -0,0 +1,15 @@
+"use strict";
+
+// TODO Variables
+var myName;
+
+// Primitive Data Types
+var undefined = "unassigned variables have this value and type"
+var dtNull = "a special keyword denoting a lack of a value"
+var boolean = true/false;
+var string = "";
+var symbol = "cannot be changed and are unique. can be a key in an object";
+var bigint = "afely store and operate on large integers even beyond the safe integer limit for Numbers.";
+var number = 123;
+var object = {firstName:"John", lastName:"Doe", age:50, eyeColor:"blue"};
+
