00d8274023023b5f39b62016d034573e16d76057
Nia Watson <nia.d.watson@gmail.com>
2022-09-12 21:17:10 -0500

    feat. added functions

--- freecodecamp/basic-js/js-101.js
+++ freecodecamp/basic-js/js-101.js
@@ -15,10 +15,10 @@ var object = {firstName:"John", lastName:"Doe", age:50, eyeColor:"blue"};
 
 // TODO Which Type?
 
-//     A term or phrase typed into a search box? string
-//     If a user is logged in? boolean
-//     A discount amount to apply to a user's shopping cart? number
-//     Whether or not a coupon code is valid? boolean
+//     A term or phrase typed into a search box? - string
+//     If a user is logged in?- boolean
+//     A discount amount to apply to a user's shopping cart? - number
+//     Whether or not a coupon code is valid? - boolean
 //     An email address typed into a registration form? string
 //     The price of a product? number
 
