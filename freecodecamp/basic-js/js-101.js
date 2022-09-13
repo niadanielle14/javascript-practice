04c13a589f83931824c06e8a4ed64f6ec8a29dac
Nia Watson <nia.d.watson@gmail.com>
2022-09-12 21:13:26 -0500

    feat. starting functions

--- freecodecamp/basic-js/js-101.js
+++ freecodecamp/basic-js/js-101.js
@@ -1,6 +1,6 @@
 "use strict";
 
-// TODO Variables
+// TODO DATA TYPES
 var myName;
 
 // Primitive Data Types
@@ -12,4 +12,15 @@ var symbol = "cannot be changed and are unique. can be a key in an object";
 var bigint = "afely store and operate on large integers even beyond the safe integer limit for Numbers.";
 var number = 123;
 var object = {firstName:"John", lastName:"Doe", age:50, eyeColor:"blue"};
+
+// TODO Which Type?
+
+//     A term or phrase typed into a search box? string
+//     If a user is logged in? boolean
+//     A discount amount to apply to a user's shopping cart? number
+//     Whether or not a coupon code is valid? boolean
+//     An email address typed into a registration form? string
+//     The price of a product? number
+
+// TODO FUNCTIONS
 
